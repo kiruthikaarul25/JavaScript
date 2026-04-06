c59448c3b5ad2f0d86a6a09fb0c85f2f8ae9297a
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 11:11:13 +0530

    Update index.js

--- Spread and Rest Operator/index.js
+++ Spread and Rest Operator/index.js
@@ -1,5 +1,5 @@
 let a=[1,2,3]
-let b=[...a] // (...) it copies all 'a' values
+let b=[...a] 
 console.log(b);
 
 let c=[10,20,30]
@@ -9,7 +9,7 @@ console.log(e);
 
 let emp={
     eId:101,
-    eName:"Nivi"
+    eName:"Keerthi"
 }
 let emp2={...emp}
 console.log(emp2);
@@ -17,9 +17,9 @@ console.log(emp2);
 let colors=["red","blue","yellow","white"]
 let[primary,secondary,...others]=colors
 
-console.log(primary); //take first value
-console.log(secondary); //take second value
-console.log(others); //print remaining values
+console.log(primary); 
+console.log(secondary); 
+console.log(others); 
 
 let emp3={
     eId:101,
