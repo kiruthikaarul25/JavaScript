8dc3a96da0861e44ebcd92d0d5f34577ecd9507a
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 09:39:12 +0530

    Add files via upload

---
+++ Var Let Const/Let/index.js
@@ -0,0 +1,27 @@
+//Declaration
+let a;
+console.log(a);
+
+//Initialization
+let b;
+b=20;
+console.log(b);
+
+//Declaration and Initialization at the same time
+let c=10;
+console.log(c);
+
+//Re-Initialization
+let d;
+d=10;
+d=20;
+console.log(d);
+
+//Re-Declaration and Re-Initialization at the same time
+// let e="Egg";
+// let e="Hen";        //we can't write same varaiable name in let
+// console.log(e);
+
+//Hoisting
+console.log(f);      
+let f="Fish";     // we can't access before write print satement
