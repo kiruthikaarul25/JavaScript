2b68714a812b08bfcb6e69f803ae3a61cc2cb9db
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 10:56:58 +0530

    Update index.js

--- Loops/index.js
+++ Loops/index.js
@@ -1,19 +1,20 @@
 let i=0;
 let n=5;
 
-// //While loop
-// while(i<n){ //condition check staring
-//     console.log(i);
-//     i++;
-// }
+while(i<n){
+    console.log(i);
+    i++;
+}
 
-// //do while loop
-// do{
-//     console.log(i);
-//     i++;
-// }while(i<=n); //condition check ending
 
-//for loop
+
+do{
+    console.log(i);
+    i++;
+}while(i<=n); 
+
+
+
 for(let i=0;i<50;i++){
     console.log(i); 
 }
