943794a6425f3b97729a65c989b8b33c94ffd3b7
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 09:59:04 +0530

    Add files via upload

---
+++ Loops/index.js
@@ -0,0 +1,19 @@
+let i=0;
+let n=5;
+
+// //While loop
+// while(i<n){ //condition check staring
+//     console.log(i);
+//     i++;
+// }
+
+// //do while loop
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=n); //condition check ending
+
+//for loop
+for(let i=0;i<50;i++){
+    console.log(i); 
+}
