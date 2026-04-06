f72fa1874ce10eb15e7f2704ccd4a18fe941c6d7
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 11:15:27 +0530

    Update index.js

--- Var Let Const/Const/index.js
+++ Var Let Const/Const/index.js
@@ -1,27 +1,27 @@
 //Declaration
-// const a;
-// console.log(a);
+const a;
+console.log(a);
 
 //Initialization
-// const b;
-// b=20;
-// console.log(b);
+const b;
+b=20;
+console.log(b);
 
 //Declaration and Initialization at the same time
-const c=10;            //this only accepted the const code
+const c=10;           
 console.log(c);
 
 //Re-Initialization
-// const d;
-// d=10;
-// d=20;
-// console.log(d);
+const d;
+d=10;
+d=20;
+console.log(d);
 
 //Re-Declaration and Re-Initialization at the same time
-// const e="Egg";
-// const e="Hen";
-// console.log(e);
+const e="Egg";
+const e="Hen";
+console.log(e);
 
 //Hoisting
-// console.log(f);
-// const f="Fish";
+console.log(f);
+const f="Fish";
