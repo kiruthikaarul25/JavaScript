800e9b2bf1129914f3288623601fdc363a14b28a
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 09:59:55 +0530

    Add files via upload

---
+++ DOM/AddEventListerner/index.js
@@ -0,0 +1,11 @@
+let d=document.getElementById('d')
+d.addEventListener('click',()=>{
+    document.body.style.background="black"
+    document.body.style.color="white"
+})
+
+let l=document.getElementById('l')
+l.addEventListener('dblclick',()=>{
+    document.body.style.background="White"
+    document.body.style.color="black"
+})
