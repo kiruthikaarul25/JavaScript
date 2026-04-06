095c8cccb7fe3a6a7d19e55708db1d1251cb47a1
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 09:45:57 +0530

    Add files via upload

---
+++ Primitive Data Types/String/index.js
@@ -0,0 +1,12 @@
+let a="I'm Darlaa";
+console.log(a);
+
+let b='I am "Darlaa"';
+console.log(b);
+
+let c=`I'm "Darlaa"`;
+console.log(c);
+
+let name="Darlaa";
+let place="Tamilnadu";  
+console.log(`My name is ${name} My Place is ${place}`);  // ${} interpolation `` template literal
