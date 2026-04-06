2d04daafc2ad3d2cd1442c49818cc29ac3541b28
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 09:37:13 +0530

    Add files via upload

---
+++ Var Let Const/Var/index.js
@@ -0,0 +1,28 @@
+//Declaration
+var a;
+console.log(a);     //there is no defined value so undefinend
+
+//Initialization
+var b;
+b=20;
+console.log(b);
+
+//Declaration and Initialization at the same time
+var c=10;
+console.log(c);
+
+//Re-Initialization  //re-intialization means update the value
+var d;
+d=10;
+d=20;
+console.log(d);
+
+//Re-Declaration and Re-Initialization at the same time
+var e="Egg";
+var e="Hen";
+console.log(e);
+
+//Hoisting
+console.log(f)
+var f="Fish";
+
