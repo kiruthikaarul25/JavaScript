6ef64d69656e02ad1f597972c0b7502c72267c83
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 11:10:28 +0530

    Refactor promise examples and add new functionality

--- Promises/index.js
+++ Promises/index.js
@@ -1,42 +1,42 @@
-// let job=true;
+let job=true;
 
-// let pro=new Promise((resolve,reject)=>{
-//     if (job) {
-//         resolve("I got a job")
-//     } else {
-//         reject("No, I didn't ")
-//     }
-// })
-// .then((res)=>console.log(res)) //condition true
-// .catch((err)=>console.log(err)) //condition false
-// .finally(()=>console.log("This is a result")); // true or false it will execute
+let pro=new Promise((resolve,reject)=>{
+    if (job) {
+        resolve("I got a job")
+    } else {
+        reject("No, I didn't ")
+    }
+})
+.then((res)=>console.log(res))
+.catch((err)=>console.log(err))
+.finally(()=>console.log("This is a result")); 
 
 let p1=Promise.reject("Hello")
 let p2=Promise.resolve("Hii")
 let p3=Promise.resolve("Hey")
 
-// Promise.any([p1,p2])
-// .then((res)=>console.log(res));
-// .catch((err)=>console.log(err));
+Promise.any([p1,p2])
+.then((res)=>console.log(res));
+.catch((err)=>console.log(err));
 
-// Promise.all([p1,p2])
-// .then((res)=>console.log(res));
-// .catch((err)=>console.log(err));
+Promise.all([p1,p2])
+.then((res)=>console.log(res));
+.catch((err)=>console.log(err));
 
-// const pro1=new Promise((resolve)=>{
-//     setTimeout()=>resolve("first"),2000;
-// });
+const pro1=new Promise((resolve)=>{
+    setTimeout()=>resolve("first"),2000;
+});
 
-// const pro2=new Promise((resolve)=>{
-//     setTimeout()=>resolve("second"),1000;
-// });
+const pro2=new Promise((resolve)=>{
+    setTimeout()=>resolve("second"),1000;
+});
 
-// const pro3=new Promise((resolve,reject)=>{
-//     setTimeout()=>reject("third"),3000;
-// });
+const pro3=new Promise((resolve,reject)=>{
+    setTimeout()=>reject("third"),3000;
+});
 
-// Promise.race([pro1,pro2])
-// .then((result)=>{console.log(result);})
+Promise.race([pro1,pro2])
+.then((result)=>{console.log(result);})
 
 Promise.allSettled([pro1,pro2,pro3])
 .then((res)=>console.log(res))
