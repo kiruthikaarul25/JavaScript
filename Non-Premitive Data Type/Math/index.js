cf25430136903792eaac24862151b751ae2a7f4f
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 09:54:09 +0530

    Add files via upload

---
+++ Non-Premitive Data Type/Math/index.js
@@ -0,0 +1,29 @@
+let a=Math.random()
+console.log(a);
+
+let b=Math.round(5.4)
+console.log(b);
+
+let c=Math.ceil(5.1) //they give high nearst whole value 
+console.log(c);
+
+let d=Math.floor(1.5) //they give lowest nearst whole value
+console.log(d);
+
+let e=Math.pow(2,3)
+console.log(e); 
+
+let f=Math.sqrt(25)
+console.log(f);
+
+let g=Math.max(4,6,9,6,1)
+console.log(g);
+
+let h=Math.min(4,6,9,6,1)
+console.log(h);
+
+let j=Math.abs(-4.6) //negative value convert into postive value
+console.log(j);
+
+
+
