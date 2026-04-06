a4df105e996be1d9f93b83139d7430aedaca6be7
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 10:51:38 +0530

    Update index.js

--- Basic/index.js
+++ Basic/index.js
@@ -1,6 +1,4 @@
-//js is not case senitive so semi colon(;) is not mandatory
-
-var a="NIVI"
+var a="Keerthi"
 console.log(a);
-document.writeln("Hello can u see me")  // ln is use for 1 tab of space to the next sentences
+document.writeln("Hello can u see me")
 document.write("Yeah i can see you") 
