7d4f0105284475a2952211a37c3edaf989278f8e
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 11:09:41 +0530

    Update index.js

--- Primitive Data Types/String/index.js
+++ Primitive Data Types/String/index.js
@@ -1,12 +1,12 @@
-let a="I'm Darlaa";
+let a="I'm Keerthi";
 console.log(a);
 
-let b='I am "Darlaa"';
+let b='I am "Keerthi"';
 console.log(b);
 
-let c=`I'm "Darlaa"`;
+let c=`I'm "Keerthi"`;
 console.log(c);
 
-let name="Darlaa";
+let name="Keerthi";
 let place="Tamilnadu";  
-console.log(`My name is ${name} My Place is ${place}`);  // ${} interpolation `` template literal
+console.log(`My name is ${name} My Place is ${place}`);  
