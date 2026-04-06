b13fcae44a4d638b44ecdbba119a329bd3886994
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 09:35:22 +0530

    Add files via upload

---
+++ Basic/index.js
@@ -0,0 +1,6 @@
+//js is not case senitive so semi colon(;) is not mandatory
+
+var a="NIVI"
+console.log(a);
+document.writeln("Hello can u see me")  // ln is use for 1 tab of space to the next sentences
+document.write("Yeah i can see you") 
