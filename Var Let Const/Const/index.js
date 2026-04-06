ca1f21b98a0caa79adae65478c8fc46c7023c436
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 09:40:49 +0530

    Add files via upload

---
+++ Var Let Const/Const/index.js
@@ -0,0 +1,27 @@
+//Declaration
+// const a;
+// console.log(a);
+
+//Initialization
+// const b;
+// b=20;
+// console.log(b);
+
+//Declaration and Initialization at the same time
+const c=10;            //this only accepted the const code
+console.log(c);
+
+//Re-Initialization
+// const d;
+// d=10;
+// d=20;
+// console.log(d);
+
+//Re-Declaration and Re-Initialization at the same time
+// const e="Egg";
+// const e="Hen";
+// console.log(e);
+
+//Hoisting
+// console.log(f);
+// const f="Fish";
