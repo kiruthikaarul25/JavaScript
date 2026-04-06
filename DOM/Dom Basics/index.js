1d83f2c3ac543b9da2880b20bfd7523149c24377
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 10:01:21 +0530

    Add files via upload

---
+++ DOM/Dom Basics/index.js
@@ -0,0 +1,9 @@
+console.log(window);
+console.log(window.document);
+console.log(document.head);
+console.log(document.body);
+console.log(window.screen.availHeight);
+console.log(window.screen.availWidth);
+console.log(window.location);
+console.log(window.navigator.geolocation);
+
