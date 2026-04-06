e5f9d5a4a53433e8820bbd3a93b434182b34b674
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 11:11:45 +0530

    Delete Storage/Local/Session directory

--- Storage/Local/Session/index.js
+++
@@ -1,9 +0,0 @@
-sessionStorage.setItem('name',"sudesh")
-sessionStorage.setItem('age',25)
-
-// sessionStorage.clear()
-// sessionStorage.removeItem('age')
-
-let name=sessionStorage.getItem('name')
-let age=sessionStorage.getItem('age')
-document.writeln(`My name is ${name} and my age is ${age}`);
