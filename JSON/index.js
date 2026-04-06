bf60615ea0b1c172ed1ed6ec8dafc4b65607f5a8
Kiruthika A <kiruthikaarul25@gmail.com>
2026-04-06 10:05:06 +0530

    Add files via upload

---
+++ JSON/index.js
@@ -0,0 +1,9 @@
+let emp={
+    eName:"Nivi",
+    eID:143,
+    ePlace:"Tamilnadu"
+}
+console.log(emp);
+console.log(JSON.stringify(emp)); //(Stringyfy) Everything into strings formate
+console.log(JSON.parse(JSON.stringify(emp))); // (parse) String into normal formate
+
